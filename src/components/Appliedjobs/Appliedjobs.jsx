15ab505910dad9dc28bed43650acfdbba00c2058
tahia <tahiamustofa@gmail.com>
2025-03-04 05:51:39 +0600

    final  message

--- src/components/Appliedjobs/Appliedjobs.jsx
+++ src/components/Appliedjobs/Appliedjobs.jsx
@@ -5,6 +5,7 @@ import { getStoredApplication } from "../Utility/localstorage";
 const AppliedJobs = () => {
     const jobs = useLoaderData();
     const [applied, setApplied] = useState([]);
+    const [display, setDisplay] = useState([]);
 
     useEffect(() => {
         const getLocal = getStoredApplication();
@@ -26,14 +27,31 @@ const AppliedJobs = () => {
 
             console.log("All Jobs Found:", all);
             setApplied(all);
+            setDisplay(all);
         } else {
             console.warn("Jobs data is not available or invalid.");
         }
     }, [jobs]);
 
+
+    const handleFilter = filter => {
+        if (filter === 'all') {
+            setDisplay(applied)
+        } else if (filter === 'remote'){
+            const remoteJobs = applied.filter(job => job.remote_or_onsite === 'Remote') ;
+            setDisplay(remoteJobs)
+        }
+         else if (filter === 'onsite'){
+            const onsiteJobs = applied.filter(job => job.remote_or_onsite === 'Onsite') ;
+            setDisplay(onsiteJobs)
+        } else{
+            'ntg';
+        }
+    }
+
     return (
         <div>
-            <div className="flex gap-10 justify-between">
+            <div className="flex gap-10 justify-between px-20">
                 <div className="p-5">
                 <p>Applied Jobs</p>
                 <h2>Number of Applied Jobs: {applied.length}</h2>
@@ -42,9 +60,9 @@ const AppliedJobs = () => {
             <details className="dropdown">
                 <summary className="btn m-1">open or close</summary>
                 <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                    <li><a>All</a></li>
-                    <li><a>onsite</a></li>
-                    <li><a>remote</a></li>
+                    <li onClick={()=>handleFilter('all')}><a>All</a></li>
+                    <li onClick={()=>handleFilter('remote')}><a> Remote </a></li>
+                    <li onClick={()=>handleFilter('onsite')}><a> Onsite </a></li>
                 </ul>
             </details>
             </div>
@@ -52,7 +70,7 @@ const AppliedJobs = () => {
 
             {applied.length > 0 && (
                 <ul className="bg-amber-100 p-10  text-black space-y-2">
-                    {applied.map((job) => ( <div key={job.id} className="bg-pink-600 p-5">
+                    {display.map((job) => ( <div key={job.id} className="bg-pink-600 p-5">
                         <li className="text-2xl" key={job.id}> {job.job_title} </li>
                         <p key={job.id}> {job.company_name}</p>
                         <p key={job.id}> {job.remote_or_onsite}</p>
